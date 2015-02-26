b48f7d09765778c5fb69de21e817906863a04e8f
diogobeato <diogo.vecchiati@gmail.com>
2015-02-25 22:53:49 -0300

    improve improve ;)

--- app/controllers/improvements_controller.rb
+++ app/controllers/improvements_controller.rb
@@ -48,7 +48,8 @@ class ImprovementsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_improvement
-      @improvement = Improvement.find(params[:id])
+      @project = Project.find(params[:project_id])
+      @improvement = @project.improvements.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
