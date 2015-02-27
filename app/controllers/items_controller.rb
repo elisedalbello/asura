c7988661d52bed261ca6def13ceae2d3ebde24f0
diogobeato <diogo.vecchiati@gmail.com>
2015-02-27 09:03:35 -0300

    updates

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   def create
-  	@improvement = Improvement.find(params[:improvement_id])	
+  	@improvement = Improvement.find(params[:improvement_id])
     @item = @improvement.items.create(item_params)
     redirect_to project_improvement_path(params[:project_id], @improvement), notice: 'Improvement was successfully created.'
   end
--- app/models/information.rb
+++ app/models/information.rb
@@ -1,3 +1,7 @@
 class Information < ActiveRecord::Base
 	belongs_to :item
+
+	def self.name_from_items(items)
+		Information.select("information.name").where("information.item_id in (?) and information.name is not null",items.map(&:id)).distinct
+	end
 end
